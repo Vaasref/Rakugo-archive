d863066a3844da8c4f78924a93f67f861435be4b
Jeremi <jeremi360@gmail.com>
2020-09-27 11:34:26 +0200

    Remove old tests, godot update scenes IDK why

---
+++ addons/Rakugo/tests/NewDialogueTest.gd
@@ -0,0 +1,14 @@
+extends Dialogue
+
+func some_dialog():
+	start_event("some_dialog")
+	
+
+	print("starting the event")
+	for i in range(10):
+		print("Say #%s" % [str(i)])
+		say({"what": "Say #%s" % [str(i)]})
+		step()
+	print("ending the event")
+	
+	end_event()
